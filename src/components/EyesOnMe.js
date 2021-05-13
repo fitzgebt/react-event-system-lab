ee7adf68376e870ef9793e4535cafd0a9e93a57b
fitzgebt <fitzgebt@gmail.com>
2021-05-13 12:42:57 -0700

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,23 @@
+import React,  { Component } from "react";
+
 // Code EyesOnMe Component Here
+export default class EyesOnMe extends Component {
+
+    handleOnFocus = (e) => {
+        console.log('Good!')
+    }
+
+    handleOnBlur = (e) => {
+        console.log('Hey! Eyes on me!')
+    }
+
+    render() {
+        return (
+            <button 
+                onFocus={this.handleOnFocus}
+                onBlur={this.handleOnBlur}
+            />
+        )
+    }
+
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,18 @@
+import React, { Component } from "react";
+
 // Code Keypad Component Here
+export default class Keypad extends Component {
+
+    handleKeyUp = (e) => {
+        console.log('Entering password...')
+    }
+
+    render() {
+        return (
+        <input 
+        type="password"
+        onKeyUp={this.handleKeyUp}
+        />
+        )
+    }
+}
